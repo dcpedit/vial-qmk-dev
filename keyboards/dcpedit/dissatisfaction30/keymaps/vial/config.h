7d255c0452ceaa332cc25e576b1118e1d9d2a575
dcpedit <dcpedit@hotmail.com>
2022-07-07 15:00:43 -0700

    Dissatisfaction 30

--- keyboards/dcpedit/dissatisfaction30/keymaps/vial/config.h
+++ keyboards/dcpedit/dissatisfaction30/keymaps/vial/config.h
@@ -1,5 +1,5 @@
 /* SPDX-License-Identifier: GPL-2.0-or-later */
 #pragma once
-#define VIAL_KEYBOARD_UID {0xA4, 0xF5, 0xF1, 0x00, 0xD3, 0xCC, 0x30, 0x72}
+#define VIAL_KEYBOARD_UID {0x0D, 0x01, 0x07, 0x16, 0x3F, 0x3D, 0x56, 0xD8}
 #define VIAL_ENCODER_DEFAULT { KC_VOLD, KC_VOLU, KC_PGDN, KC_PGUP }
 #define DYNAMIC_KEYMAP_LAYER_COUNT 2
