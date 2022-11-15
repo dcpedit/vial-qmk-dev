bb2aac777c3aaeb236fa05e11eec921370c6ea1f
dcpedit <dcpedit@hotmail.com>
2022-11-14 20:19:36 -0800

    Sketchy Sketchy firmware

--- keyboards/dcpedit/sketchy/rules.mk
+++ keyboards/dcpedit/sketchy/rules.mk
@@ -9,7 +9,6 @@ BOOTLOADER = atmel-dfu
 #
 BOOTMAGIC_ENABLE = yes      # Virtual DIP switch configuration
 MOUSEKEY_ENABLE = yes       # Mouse keys
-POINTING_DEVICE_ENABLE = yes
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
